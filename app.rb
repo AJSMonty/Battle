da29bc2e2cb30f23f1b42afd30eb665916365a7d
AJ <aj.monty@gmail.com>
2020-12-07 17:16:19 +0000

    Cathal & AJ's first html form

--- app.rb
+++ app.rb
@@ -9,8 +9,13 @@ get '/secret' do
   "Ssshhhhhh please"
 end
 
-get '/cat' do
-    @generator = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
     erb(:index)
+end
 
+get '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
 end
