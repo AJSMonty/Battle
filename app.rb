4e65f0ac27dcb4c971778da02b80f5d205c355fe
AJ <aj.monty@gmail.com>
2020-12-07 16:13:38 +0000

    Added cat pic with red border

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
-    "hello! world"
+    "Hello world!"
 end
 
 get '/secret' do
-  "ssshhhhhh please"
+  "Ssshhhhhh please"
+end
+
+get '/cat' do
+    "<div>
+    <img src='https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg' style='border: 3px dashed red'>
+    </div>"
 end
