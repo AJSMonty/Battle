9a961ae5eaeb167d77af60ec123c0aba60879d01
ZeenLamDev <Zeen.Lam93@gmail.com>
2020-12-08 16:34:29 +0000

    added form to take player 1 and 2, sends post request and redirects to /names

--- battle-challenge/spec/features/testing_infrastructure_spec.rb
+++ battle-challenge/spec/features/testing_infrastructure_spec.rb
@@ -6,3 +6,16 @@ feature 'Testing infrastructure' do
       expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
+feature 'Adding Players' do
+  scenario 'Page renders form' do
+    visit('/')
+    within('form') do
+      fill_in 'player1', with: 'Zeen'
+      fill_in 'player2', with: 'AJ'
+    end
+    click_button('Submit')
+    expect(page).to have_content 'Welcome player 1: Zeen'
+    expect(page).to have_content 'Welcome player 2: AJ'
+  end
+end
